5e8a1f9f1e2a26af2e0c2c2770db2bd84177b8ec
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-11-24 15:51:12 -0300

    add all the data and the add the seeds

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/yelpCamp")
-//   .then(() => {
-//     console.log("Mongo connection open");
-//   })
-//   .catch((err) => {
-//     console.log("Something went wrong when connecting to mongo");
-//     console.log(err);
-//   });
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp"),
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  };
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error connecting to database"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/newcamp", async (req, res) => {
+  const camp = new Campground({ title: "Casa Vó", location: "São Miguel" });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
