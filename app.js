653e3fc983b778f3ad2d8d574328e8f0779314c6
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-11-25 15:44:15 -0300

    Fixing show bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp"),
@@ -17,25 +18,21 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.set("view engine", "ejs");
 
 app.get("/campgrounds", async (req, res) => {
   const camps = await Campground.find({});
   res.render("campgrounds/index", { camps });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
   const camp = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { camp });
-});
-
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
 });
 
 app.listen(3000, () => {
