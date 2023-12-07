46ce403653eb248c5ed35012fb589cb1e3c94af2
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-12-07 15:16:52 -0300

    Adding new validators for page not found and invalid Data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -46,6 +47,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   wrapAsync(async (req, res) => {
+    if (!req.body.campground)
+      throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -88,8 +91,13 @@ app.delete(
   })
 );
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("sum went wrong");
+  const {statusCode = 500, message = "Suomething went wrong"} = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
