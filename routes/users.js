d94777806a031ac6edd1ed30f7c857d1c0e6552f
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-01-22 19:19:11 -0300

    Adding logout

--- routes/users.js
+++ routes/users.js
@@ -40,4 +40,13 @@ router.post(
   }
 );
 
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Goodbye!");
+    res.redirect("/campgrounds");
+  });
+});
 module.exports = router;
