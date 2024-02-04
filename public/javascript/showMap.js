6ce81047d6793829e41d13c9bc322d64b37cb398
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-02-04 18:23:31 -0300

    Adding a popup on the map marker and changing the map style

--- public/javascript/showMap.js
+++ public/javascript/showMap.js
@@ -1,9 +1,14 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/light-v10", // stylesheet location
+  style: "mapbox://styles/mapbox/streets-v10", // stylesheet location
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ofset: 25}).setHTML(`<h5>${campground.title}</h5>`)
+  )
+  .addTo(map);
