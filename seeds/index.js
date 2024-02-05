b6fc95ba7354113b5f7336bdbbe5821148b7d2f9
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-02-05 09:38:45 -0300

    Associating the cluster map with the capmgrounds data

--- seeds/index.js
+++ seeds/index.js
@@ -32,7 +32,10 @@ const seedDB = async () => {
       price,
       geometry: {
         type: "Point",
-        coordinates: [-48.4703, -27.6019],
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
       },
       images: [
         {
