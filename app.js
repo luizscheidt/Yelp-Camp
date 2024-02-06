c9fd3d07889992608806ae6a0dc656aa1adfad93
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-02-06 09:49:38 -0300

    Adding express-mongo-sanitize for security against mongo injections

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const methodOverride = require("method-override");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
@@ -39,6 +40,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: "Secreto",
@@ -61,6 +63,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
