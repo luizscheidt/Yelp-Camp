f4db8ef640494410cc03729edf9e637fa86bfbcf
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-02-08 17:08:21 -0300

    changing the mongo connection to only atlas

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRoutes = require("./routes/reviews");
 
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
